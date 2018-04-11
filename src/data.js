301d99f6ccbeafe770b87db1325aa83e8a27a71b
ohadgreen <ogreen@ebay.com>
2018-04-11 20:52:03 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+//data.js
+const data = [
+    { id: 1, author: 'Avraham', text: 'Wow this is neat!'},
+    { id: 2, author: 'Itzhak', text: 'youre __right!__' },
+    { id: 3, author: 'Jacob', text: 'Halleluya' },
+    { id: 4, author: 'Moses', text: 'Having fun in Sinay' }
+]
+module.exports = data;
